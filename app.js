f4db36450188ab45ee828046004b3d2c35abc2d3
Nicu <nicolae.taban@gmail.com>
2021-10-16 16:41:54 +0300

    Adds basic campground schema

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const path = require("path");
+const mongoose = require('mongoose');
+const path = require('path');
+const Campground = require('./models/campground');
+mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => console.log('Mongo connection')).catch((error) => console.log(error));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
 const app = express();
-const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -10,6 +19,15 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/makeCampground', async (req, res) => {
+    const camp = new Campground({
+        title: 'My Backyard',
+        description: 'Cheap Camping!'
+    });
+    await camp.save();
+    res.send(camp);
+})
+const port = 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`);
 });
