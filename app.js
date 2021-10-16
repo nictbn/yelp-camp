f890f34c8d4ae1e68484178d4ce8291da75375d3
Nicu <nicolae.taban@gmail.com>
2021-10-16 17:37:21 +0300

    Adds campgrounds index

--- app.js
+++ app.js
@@ -20,14 +20,11 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/makeCampground', async (req, res) => {
-    const camp = new Campground({
-        title: 'My Backyard',
-        description: 'Cheap Camping!'
-    });
-    await camp.save();
-    res.send(camp);
-})
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`);
