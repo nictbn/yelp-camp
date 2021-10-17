1e0ad1e376df02713494dfd41f02b2b786c53312
Nicu <nicolae.taban@gmail.com>
2021-10-17 15:31:44 +0300

    Add images to camps

--- seeds/index.js
+++ seeds/index.js
@@ -15,9 +15,13 @@ const seedDb = async () => {
     await Campground.deleteMany();
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Paleo meh disrupt, banjo officia labore cupidatat ethical. Cray seitan keytar PBR&B. Ennui narwhal bespoke laboris. Qui eu normcore occupy direct trade single-origin coffee umami banh mi twee do gastropub truffaut. Consequat laboris helvetica, ennui selvage williamsburg blog roof party sustainable glossier ethical messenger bag. 90\'s ugh authentic esse, palo santo exercitation actually edison bulb health goth subway tile kombucha.',
+            price: price,
         });
         await camp.save();
     }
