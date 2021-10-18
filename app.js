62c6848dc8860187dfd4b4f4f8e563d9cc1a8eec
Nicu <nicolae.taban@gmail.com>
2021-10-18 23:21:24 +0300

    Use JOI to validate campground creation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ Campground = require('./models/campground');
 mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => console.log('Mongo connection')).catch((error) => console.log(error));
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 
@@ -38,8 +39,19 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) {
-        throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required(),
+        }).required()
+    });
+    const {error} = campgroundSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(el => el.message).join(',');
+        throw new ExpressError(message, 400);
     }
     const campground = new Campground(req.body.campground);
     await campground.save();
