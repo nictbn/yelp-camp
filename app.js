36b2ced2cb9d69b88411df3b325940878841e208
Nicu <nicolae.taban@gmail.com>
2021-10-24 15:45:09 +0300

    Create Reviews

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const path = require('path');
 let Campground;
 Campground = require('./models/campground');
+let Review;
+Review = require('./models/review');
 mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => console.log('Mongo connection')).catch((error) => console.log(error));
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
@@ -74,6 +76,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
