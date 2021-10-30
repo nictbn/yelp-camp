44a205678b33f5767f21f88edb1acd1fe3ff4dd0
Nicu <nicolae.taban@gmail.com>
2021-10-30 19:47:23 +0300

    Adds an author to the Campground

--- seeds/index.js
+++ seeds/index.js
@@ -17,6 +17,7 @@ const seedDb = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground({
+            author: '617aeda40e0a416dc3876e9a',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
