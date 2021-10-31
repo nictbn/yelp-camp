2900f9f5f03d75dc98c1bf023248a0d810167951
Nicu <nicolae.taban@gmail.com>
2021-10-31 14:40:17 +0200

    Fixes seeds script and index page

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers')
+const {places, descriptors} = require('./seedHelpers')
 let Campground;
 Campground = require('./../models/campground');
 mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => console.log('Mongo connection')).catch((error) => console.log(error));
@@ -20,9 +20,22 @@ const seedDb = async () => {
             author: '617aeda40e0a416dc3876e9a',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Paleo meh disrupt, banjo officia labore cupidatat ethical. Cray seitan keytar PBR&B. Ennui narwhal bespoke laboris. Qui eu normcore occupy direct trade single-origin coffee umami banh mi twee do gastropub truffaut. Consequat laboris helvetica, ennui selvage williamsburg blog roof party sustainable glossier ethical messenger bag. 90\'s ugh authentic esse, palo santo exercitation actually edison bulb health goth subway tile kombucha.',
             price: price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/drchkiqdc/image/upload/v1635683367/YelpCamp/xuscngvkjlhdx6d5zznd.jpg',
+                    filename: 'YelpCamp/xuscngvkjlhdx6d5zznd',
+                },
+                {
+                    url: 'https://res.cloudinary.com/drchkiqdc/image/upload/v1635683367/YelpCamp/ix3qfcfiumgjjgrjduik.jpg',
+                    filename: 'YelpCamp/ix3qfcfiumgjjgrjduik',
+                },
+                {
+                    url: 'https://res.cloudinary.com/drchkiqdc/image/upload/v1635683367/YelpCamp/otpmfbg8zgkcvjxulmdy.jpg',
+                    filename: 'YelpCamp/otpmfbg8zgkcvjxulmdy',
+                }
+            ],
         });
         await camp.save();
     }
