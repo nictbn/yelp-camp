d417f0921aeaf29195a14dcadb9effb7637ba713
Nicu <nicolae.taban@gmail.com>
2021-11-01 23:12:30 +0200

    Centers the map on a campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,9 @@
 mapboxgl.accessToken = mapToken;
+console.log(campground.geometry.coordinates);
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
